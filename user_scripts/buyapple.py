4af0f9ff8b1a9bc5c86548cee856a505f46e542a
Marvin Liu <marvinliu9@gmail.com>
2015-09-14 23:13:03 -0400

    update buyapple.py

--- user_scripts/buyapple.py
+++ user_scripts/buyapple.py
@@ -1,20 +1,38 @@
-from zipline.api import order, symbol
+from zipline.api import order_target, record, symbol, history, add_history
 
-# Put any initialization logic here.  The context object will be passed to
-# the other methods in your algorithm.
+
 def initialize(context):
-    context.stocks = symbols('aapl')
+    # Register 2 histories that track daily prices,
+    # one with a 100 window and one with a 300 day window
+    add_history(100, '1d', 'price')
+    add_history(300, '1d', 'price')
 
-# Will be called on every trade event for the securities you specify. 
+    context.i = 0
+
+
 def handle_data(context, data):
-    # Implement your algorithm logic here.
+    # Skip first 300 days to get full windows
+    context.i += 1
+    if context.i < 300:
+        return
 
-    # data[sid(X)] holds the trade event data for that security.
-    # context.portfolio holds the current portfolio state.
+    # Compute averages
+    # history() has to be called with the same params
+    # from above and returns a pandas dataframe.
+    short_mavg = history(100, '1d', 'price').mean()
+    long_mavg = history(300, '1d', 'price').mean()
 
-    # Place orders with the order(SID, amount) method.
+    sym = symbol('AAPL')
 
-    # TODO: implement your own logic here.
-    order(symbol('aapl'), 50)
-    print(context.portfolio.portfolio_value)
+    # Trading logic
+    if short_mavg[sym] > long_mavg[sym]:
+        # order_target orders as many shares as needed to
+        # achieve the desired number of shares.
+        order_target(sym, 100)
+    elif short_mavg[sym] < long_mavg[sym]:
+        order_target(sym, 0)
 
+    # Save values for later inspection
+    record(AAPL=data[sym].price,
+           short_mavg=short_mavg[sym],
+           long_mavg=long_mavg[sym])
