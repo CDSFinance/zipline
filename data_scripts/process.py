02758b5f6d7ef98e0aad8d0631cf9d21dff880d9
Marvin Liu <marvinliu9@gmail.com>
2015-10-22 20:30:46 -0400

    finish normalization of company data

--- data_scripts/process.py
+++ data_scripts/process.py
@@ -7,7 +7,7 @@ start_year = 2000
 start_month = 1
 start_day = 1
 
-def getStartDate():
+def get_start_date():
   y = str(start_year)
   m = str(start_month)
   d = str(start_day)
@@ -22,12 +22,22 @@ def mean(col):
   c = [float(i) for i in col]
   return sum(c)/float(len(c))
 
+def stdev(col, mean):
+  c = [(float(i) - mean)**2 for i in col]
+  return (sum(c)/float(len(c) - 1))**0.5
+
+def normalize(col, mean, stdev):
+  c = [((float(i) - mean)/stdev) for i in col]
+  return c
+
 s3 = boto3.resource('s3')
-# s3.meta.client.download_file('cdsquantfinance', 'AAPL.csv', '/Users/marvin/zipline/data_scripts/AAPL.csv')
+# s3.meta.client.download_file(
+  # 'cdsquantfinance', 
+  # 'AAPL.csv', 
+  # '/Users/marvin/zipline/data_scripts/AAPL.csv')
 
 bucket = s3.Bucket(bucket_name)
 exists = True
-
 
 try:
   s3.meta.client.head_bucket(Bucket=bucket_name)
@@ -36,23 +46,61 @@ except botocore.exceptions.ClientError as e:
   if error_code == 404:
     exists = False
 
+# Processes each csv file in the bucket
 for obj in bucket.objects.limit(count = 1):
-  data = obj.get()["Body"].read()
+  # dumps csv conteents into a string
+  content_str = obj.get()["Body"].read()
+  file_name = obj.key
   
-  while data.find(getStartDate()) == -1:
+  # find the first date starting at the start date where the market is open
+  while content_str.find(get_start_date()) == -1:
     start_day += 1
   
-  startIndex = data.find(getStartDate())
-  data = data[startIndex:]
-  f = StringIO.StringIO(data)
-  reader = csv.reader(f, delimiter=',')
-  readerT = [list(i) for i in zip(*reader)]
-  readerT = readerT[1:]
+  start_i = content_str.find(get_start_date())
+  
+  # truncate the data to just the data from the start date onwards
+  content_str = content_str[start_i:]
+  
+  # parse the content into a list of lists
+  content_file = StringIO.StringIO(content_str)
+  reader = csv.reader(content_file, delimiter=',')
 
-  for col in readerT:
-    print mean(col)
-    # print '\t'.join(col)
-    
+  data_t = [list(i) for i in zip(*reader)]
+  
+  # take the date column and start the output data table
+  output_t = data_t[0:1]
+  
+  # remove the date column from the data to be normalized
+  data_t = data_t[1:]
+
+  # store the means and standard deviations for each column
+  means = []
+  stdevs = []
+
+  # normalize each column
+  for col in data_t:
+    m = mean(col)
+    s = stdev(col, m)
+    means.append(m)
+    stdevs.append(s)
+    output_t.append(normalize(col, m, s))
+
+  # transpose the output_t to get it back into row format
+  output = [list(i) for i in zip(*output_t)]
+
+  # open output file and get a csv writer
+  outfile = open(file_name, "w")
+  writer = csv.writer(outfile)
+
+  # write header and data
+  header = ["Date", "Open", "High", "Low", "Close", "Volume", "Ex-Dividend", "Split Ratio", "Adj. Open", "Adj. High", "Adj. Low", "Adj. Close", "Adj. Volume"]
+  writer.writerow(header)
+
+  [writer.writerow(x) for x in output]
+
+  # close file
+  outfile.close()
+
   
 
 
